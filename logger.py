15ef1629adae4a7520e61a403baf2a95027809bc
Moritz Welsch <moritz@welsch.pro>
2022-12-24 14:07:35 +0100

    Add newline after entry, not before

--- logger.py
+++ logger.py
@@ -4,9 +4,10 @@ import time
 
 def log(text):
     file1 = open("log", "a")  # append mode
-    file1.write("\n")
+
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     current_time += str(text)
     file1.write(current_time)
+    file1.write("\n")
     file1.close()
